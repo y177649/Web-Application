6de0d2bcc4b87420e2c8a2c0e3af4c6e4e925a42
sato <22311092ys@tama.ac.jp>
2024-05-16 00:04:59 +0900

    update app.route japan america world

--- practice/flask/hello.py
+++ practice/flask/hello.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/japan")
+def japan():
+    return "<p>Hello,Japan!<p>"
+
+@app.route("/america")
+def america():
+    return "<p>Hello,America!<p>"
+
+@app.route("/world")
+def world():
     return "<p>Hello,World!<p>"

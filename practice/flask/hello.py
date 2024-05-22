d6942fc5a52d36041f56e96bf619c5a447f7d785
y177649 <22311092ys@tama.ac.jp>
2024-05-22 10:56:08 +0000

    update flask

--- practice/flask/hello.py
+++ practice/flask/hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+"""
 @app.route("/japan")
 def japan():
     return "<p>Hello,Japan!<p>"
@@ -13,3 +14,8 @@ def america():
 @app.route("/world")
 def world():
     return "<p>Hello,World!<p>"
+"""
+
+@app.route("/japan/<city>")
+def japan(city):
+    return f"Hello, { city } in Japan!"

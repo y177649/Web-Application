04960cce8ccd7183c0c4c742a5f80363dd0c681c
sato <22311092ys@tama.ac.jp>
2024-05-23 21:06:34 +0900

    可変ルート

--- practice/flask/hello.py
+++ practice/flask/hello.py
@@ -2,20 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-"""
-@app.route("/japan")
-def japan():
-    return "<p>Hello,Japan!<p>"
-
-@app.route("/america")
-def america():
-    return "<p>Hello,America!<p>"
-
-@app.route("/world")
-def world():
-    return "<p>Hello,World!<p>"
-"""
-
-@app.route("/japan/<city>")
+@app.route("/japan/<city>/")
 def japan(city):
-    return f"Hello, { city } in Japan!"
+    return f"Hello, {city} in Japan!"
